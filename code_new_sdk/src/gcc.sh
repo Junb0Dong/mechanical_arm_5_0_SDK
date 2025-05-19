62e8b181ca461fe072a062c1015f9d36e635fd8b
Junb0Dong <junbodong0628@gmail.com>
2025-05-19 16:29:26 +0800

    modify the server_main.cpp for new sdk

--- code_new_sdk/src/gcc.sh
+++ code_new_sdk/src/gcc.sh
@@ -3,3 +3,4 @@ export CPLUS_INCLUDE_PATH=~/mechanical_arm_5_0_SDK/code_new_sdk/include:$CPLUS_I
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/mechanical_arm_5_0_SDK/code_new_sdk/include
 g++ main.cpp -L../include -I/usr/include/python3.10 -lpython3.10  -I /usr/include/eigen3 -lcontrolcan -lmylibti5 -o move_sov
 g++ move_origin.cpp -L../include -I/usr/include/python3.10 -lpython3.10  -I /usr/include/eigen3 -lcontrolcan -lmylibti5 -o move_origin
+g++ server_main.cpp robot_server.cpp -L../include -I/usr/include/python3.10 -lpython3.10  -I /usr/include/eigen3 -lcontrolcan -lmylibti5 -o robot_server
