3e1a0c0bfd3e7336e8700dafd5d156730fe2d046
Junb0Dong <junbodong0628@gmail.com>
2025-05-20 23:25:58 +0800

    finish the onnx_socket_robot code BUT not test it

---
+++ code_new_sdk/build/CMakeFiles/keyboard_controll.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/keyboard_controll.dir/src/keyboard_controll.cpp.o"
+  "CMakeFiles/keyboard_controll.dir/src/keyboard_controll.cpp.o.d"
+  "keyboard_controll"
+  "keyboard_controll.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/keyboard_controll.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ code_new_sdk/build/CMakeFiles/onnx_socket_robot.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/onnx_socket_robot.dir/src/onnx_socket_robot.cpp.o"
+  "CMakeFiles/onnx_socket_robot.dir/src/onnx_socket_robot.cpp.o.d"
+  "CMakeFiles/onnx_socket_robot.dir/src/socket_server.cpp.o"
+  "CMakeFiles/onnx_socket_robot.dir/src/socket_server.cpp.o.d"
+  "onnx_socket_robot"
+  "onnx_socket_robot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/onnx_socket_robot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
